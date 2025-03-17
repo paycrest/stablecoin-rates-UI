bfb91953b631e524a6db13baca1bcf1454db69c3
chiscookeke11 <chiscookeke11@gmail.com>
2025-03-17 18:08:50 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="bg-background">
+      <Header/>
+    </div>
   )
 }
 
