1ddac6d899914f766817f51b7e983d950dcb8775
Joseph Poveda Flores <josephpf12@hotmail.com>
2025-03-21 08:27:18 -0600

    Add footer into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="bg-background h-screen">
-    <Header/>
-  </div>;
+  return (
+    <div className="bg-background min-h-screen flex flex-col relative pb-32">
+      <Header />
+      <div className="flex-grow"></div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
