74a99ac30d38b3c22885ae6225758cafaf4f700b
David Atanda <atandadave@gmail.com>
2025-03-21 03:36:32 +0000

    fix: tweak base styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return <div className="bg-background">
+  return <div className="bg-background h-screen">
     <Header/>
   </div>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,24 +3,22 @@ import logo from "../assets/logo/logo.svg";
 
 
 const Header: React.FC = () => {
-    return (
-        <header className="w-full h-[60px] flex flex-row items-center justify-between gap-7 bg-[#444444] py-5 px-7 md:px-20">
+  return (
+    <header className="w-full h-[6rem] flex flex-row items-center justify-between gap-7 py-5 px-7 md:px-20">
+      <img
+        src={logo}
+        alt="logo"
+        className="w-[12rem] h-[1.32rem] object-contain cursor-pointer"
+      />
 
-
-            <img
-                src={logo}
-                alt="logo"
-                className="w-[120px] h-[13.26px] object-contain cursor-pointer"
-            />
-
-            <a
-                href="#"
-                className="text-[#FFFFFF80] font-medium text-base hover:text-[#FFFFFF] active:text-[#FFFFFF] transition duration-200 ease-in-out"
-            >
-                For Developers
-            </a>
-        </header>
-    );
+      <a
+        href="#"
+        className="text-white-dull text-[1.6rem] font-medium text-base hover:text-white active:text-white transition duration-200 ease-in-out"
+      >
+        For Developers
+      </a>
+    </header>
+  );
 };
 
 export default Header;
