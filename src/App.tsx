c279f8cc5dc800c0976e71e7c68661a696b2c75e
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-03-25 10:27:00 -0500

    Component name has been changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
-import CurrencyConverter from "./components/currencyConverter";
+import CurrencyConverter from "./components/currencyConverter/CurrencyConverter";
+
 
 function App() {
   return <div className="bg-background h-screen">
