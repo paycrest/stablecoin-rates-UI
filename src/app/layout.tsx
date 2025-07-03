50700c25c5bc5641ff292e3991f752a4b79d3711
David Atanda <atandadave@gmail.com>
2025-07-03 09:44:01 +0000

    fix: SEO fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,21 @@ import '@/index.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Stablecoins Rate',
+  title: 'Stablecoins Rates',
   description: 'Real-time stablecoin exchange rates and currency conversion tools',
-  keywords: 'stablecoin, cryptocurrency, exchange rates, USDT, USDC, DAI, BUSD',
+  keywords: 'stablecoin, cryptocurrency, exchange rates, USDT, USDC, DAI',
+  icons: 'https://res.cloudinary.com/dfkuxnesz/image/upload/v1751520539/paycrest_dbgtr7.svg',
   authors: [{ name: 'Stablecoin Rates' }],
   openGraph: {
     title: 'Stablecoins Rate',
-    description: 'Real-time stablecoin exchange rates and currency conversion tools',
+    description: 'Real-time stablecoin exchange rates and currency conversion',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Stablecoins Rate',
     description: 'Real-time stablecoin exchange rates and currency conversion tools',
+    images: 'https://res.cloudinary.com/dfkuxnesz/image/upload/v1751520665/Screenshot_2025-07-03_at_06.30.46_bxx3v1.png'
   },
   robots: {
     index: true,
